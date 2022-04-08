aec73998456c90a0520babba129e99d6acf721a9
1huali <wanhuali2000@gmail.com>
2022-04-08 14:39:31 -0400

    code cleanup and prev/next function works

--- wawa-ffar250/js/script.js
+++ wawa-ffar250/js/script.js
@@ -6,69 +6,74 @@ Author Name
 */
 window.onload = function (){
 
-"use strict";
-
-let playlist =[];
-let audio1= document.getElementById(`audio1`);
-let audio2= document.getElementById(`audio2`);
-let audio3= document.getElementById(`audio3`);
-let currentTrack = `audio1`;
-let previousTrack;
-let currentTrackIndex = 0;
-playlist.push(`audio1`);
-playlist.push(`audio2`);
-playlist.push(`audio3`);
-
-let nextButton= document.getElementById(`nextButton`);
-
-// window.requestAnimationFrame(animate)
-
-nextButton.addEventListener("click", function (event){
+    "use strict";
     
-currentTrack=`audio1`
-
-    for (let i=0; i < playlist.length; i++){
-
-
-currentTrack = previousTrack;
-previousTrack.stop(); 
-
-currentTrack = playlist[i+1];
-currentTrackIndex++;
-currentTrack.play();
-
-console.log(currentTrack);
-console.log(currentTrackIndex);
-     
-
-};
-
+    let playlist =[];
+    let audio1= document.getElementById(`audio1`);
+    let audio2= document.getElementById(`audio2`);
+    let audio3= document.getElementById(`audio3`);
+    
+    //set current
+    let currentTrack = audio1;
+    
+    //set previous to null
+    let previousTrack =null;
+    
+    //to keep track of who is playing
+    let currentTrackIndex = 0;
+    
+    //push the actual tracks :)
+    playlist.push(audio1);
+    playlist.push(audio2);
+    playlist.push(audio3);
+    
+    let nextButton= document.getElementById(`nextButton`);
+    
+    // window.requestAnimationFrame(animate)
+    
+    nextButton.addEventListener("click", function (event){
         
-    if (currentLookIndex === playlist.length){
-      currentLookIndex = 0;
+    //currentTrack=`audio1`
+    
+      //  for (let i=0; i < playlist.length; i++){
+    
+    if(previousTrack!==null){
+        previousTrack.pause(); 
     }
     
-        // }
-});
-
-// function animate(){
-
-// function titleChange(){
-    // track playing underlined
-    //     for (let i=0; i < playlist.length; i++){
-            //     playlist[i].element.classList.remove(playlist[currentTrackIndex]);
-
-    //   playlist[i].element.classList.add(playlist[currentTrackIndex]);
+    currentTrack.play();
+    previousTrack =currentTrack;
     
-
-
-    //   if (currentTrackIndex===3){
-    //     playlist[i].element.textContent="";
-    //   } else {
-    //     playlist[i].element.textContent=playlist[i].trackDisplayText;
-    //   }
+    currentTrackIndex++;
     
-// }
-
-// }
-}
+    if(currentTrackIndex === playlist.length){
+        currentTrackIndex =0;
+    
+    }
+    currentTrack = playlist[currentTrackIndex];
+    console.log(currentTrack);
+    console.log(previousTrack);
+         
+    });
+    
+    // function animate(){
+    
+    // function titleChange(){
+        // track playing underlined
+        //     for (let i=0; i < playlist.length; i++){
+                //     playlist[i].element.classList.remove(playlist[currentTrackIndex]);
+    
+        //   playlist[i].element.classList.add(playlist[currentTrackIndex]);
+        
+    
+    
+        //   if (currentTrackIndex===3){
+        //     playlist[i].element.textContent="";
+        //   } else {
+        //     playlist[i].element.textContent=playlist[i].trackDisplayText;
+        //   }
+        
+    // }
+    
+    // }
+    }
