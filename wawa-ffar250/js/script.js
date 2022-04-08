0ed881c6df1ed096634b6d00ab88ac57ae4689b9
1huali <wanhuali2000@gmail.com>
2022-04-08 10:16:49 -0400

    bug: next track feature

--- wawa-ffar250/js/script.js
+++ wawa-ffar250/js/script.js
@@ -2,8 +2,7 @@
 Title of Project
 Author Name
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+1. audio controls only shows at play, title  change color when is playing
 */
 window.onload = function (){
 
@@ -13,9 +12,63 @@ let playlist =[];
 let audio1= document.getElementById(`audio1`);
 let audio2= document.getElementById(`audio2`);
 let audio3= document.getElementById(`audio3`);
-let currentSong = `track1`
-song1.push(`track1`);
-song2.push(`track2`);
-song3.push(`track3`);
+let currentTrack = `audio1`;
+let previousTrack;
+let currentTrackIndex = 0;
+playlist.push(`audio1`);
+playlist.push(`audio2`);
+playlist.push(`audio3`);
 
+let nextButton= document.getElementById(`nextButton`);
+
+// window.requestAnimationFrame(animate)
+
+nextButton.addEventListener("click", function (event){
+    
+currentTrack=`audio1`
+
+    for (let i=0; i < playlist.length; i++){
+
+
+currentTrack = previousTrack;
+previousTrack.stop(); 
+
+currentTrack = playlist[i+1];
+currentTrackIndex++;
+currentTrack.play();
+
+console.log(currentTrack);
+console.log(currentTrackIndex);
+     
+
+};
+
+        
+    if (currentLookIndex === playlist.length){
+      currentLookIndex = 0;
+    }
+    
+        // }
+});
+
+// function animate(){
+
+// function titleChange(){
+    // track playing underlined
+    //     for (let i=0; i < playlist.length; i++){
+            //     playlist[i].element.classList.remove(playlist[currentTrackIndex]);
+
+    //   playlist[i].element.classList.add(playlist[currentTrackIndex]);
+    
+
+
+    //   if (currentTrackIndex===3){
+    //     playlist[i].element.textContent="";
+    //   } else {
+    //     playlist[i].element.textContent=playlist[i].trackDisplayText;
+    //   }
+    
+// }
+
+// }
 }
