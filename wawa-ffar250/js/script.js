413998ab2308ec6445fb273c377cd48eacd3831c
1huali <wanhuali2000@gmail.com>
2022-04-08 09:04:11 -0400

    html tracks into js obj

--- wawa-ffar250/js/script.js
+++ wawa-ffar250/js/script.js
@@ -5,7 +5,17 @@ Author Name
 This is a template. You must fill in the title,
 author, and this description to match your project!
 */
+window.onload = function (){
 
 "use strict";
 
-// Code goes here
+let playlist =[];
+let audio1= document.getElementById(`audio1`);
+let audio2= document.getElementById(`audio2`);
+let audio3= document.getElementById(`audio3`);
+let currentSong = `track1`
+song1.push(`track1`);
+song2.push(`track2`);
+song3.push(`track3`);
+
+}
